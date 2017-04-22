9cad74c835d5a29f21dd9ab026066969dfd3c7d3
apasashkov <a.pasashkov@gmail.com>
2017-04-22 19:12:02 +0300

    v1.02

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -31,5 +31,67 @@ function BinarySearchTree(){
         insertNode(node.right,newNode);
       }
     }
+  };
+
+  this.inOrderTraverse=function(callback){
+    inOrderTraverse(root,callback);
+  };
+
+  var inOrderTraverse=function(node,callback){
+    if(node!==null) {
+      inOrderTraverse(node.left,callback);
+      callback(node.key);
+      inOrderTraverse(node.right,callback);
+    }
+  };
+
+  this.preOrderTraverse=function(callback){
+    preOrderTraverseNode(root,callback);
+    }
+  
+  var preOrderTraverseNode=function(node,callback){
+    if(node!==null){
+      callback(node.key);
+      preOrderTraverseNode(node.left,callback);
+      preOrderTraverseNode(node.right,callback);
+    }
+  };
+
+  this.postOrderTraverse=function(callback){
+    postOrderTraverseNode(root,callback);
+  };
+
+  var postOrderTraverseNode=function(node,callback){
+    if(node!==null){
+      postOrderTraverseNode(node.left,callback);
+      postOrderTraverseNode(node.right,callback);
+      callback(node.key);
+    }
   }
-}
+};
+
+var n=new BinarySearchTree();
+n.insert(1);
+n.insert(50);
+n.insert(24);
+n.insert(3);
+n.insert(12);
+n.insert(33);
+n.insert(84);
+n.insert(32);
+n.insert(31);
+n.insert(45);
+n.insert(4);
+n.insert(5);
+
+n.inOrderTraverse(function(value){
+  console.log(value);
+})
+console.log('-----------------');
+n.preOrderTraverse(function(value){
+  console.log(value);
+})
+console.log('-----------------');
+n.postOrderTraverse(function(value){
+  console.log(value);
+})
